6a0b09725fffc09007ae59f6fb5ca8451fd7db1f
Gutu Galuppo <galuppodev@gmail.com>
2020-08-21 17:48:58 +0200

    Initial Commit

---
+++ SlotApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotApp
+//
+//  Created by Gutu Galuppo on 21.08.20.
+//  Copyright © 2020 Gutu Galuppo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
